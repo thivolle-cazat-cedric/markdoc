005993f3f04c7ab5d3834f25c9e15fca3aa8dd5d
thivolle cedric <cedric@thivolle-cazat.fr>
2015-07-23 12:39:37 +0200

    on ajout le sample de conf d'environement

---
+++ app/config/env/dev.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+#
+###############################
+# DEVELOPPEMENT CONFIGURATION #
+###############################
+#
+DEBUG = True
+
