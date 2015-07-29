ea355ea8705d8e40545034c0a2a85486a0899bb4
thivolle cedric <cedric@thivolle-cazat.fr>
2015-07-29 20:12:20 +0200

    mise en place de la colloration syntax

--- app/lib/js/main.js
+++ app/lib/js/main.js
@@ -1,5 +1,8 @@
-$(function () {
-  
+$(document).ready(function() {
   	$('[data-toggle="tooltip"]').tooltip()
+  	$('pre code').each(function(i, block) {
+  		$('pre code').parent().addClass('hljs');
+	    hljs.highlightBlock(block);
+	  });
 
-})
+});
